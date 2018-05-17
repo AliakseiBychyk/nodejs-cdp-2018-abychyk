5f8641c5ac8de19a77c4b88fa8577cee596a2d83
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-05-17 19:40:17 +0300

    implement streams methods

--- utils/streams.js
+++ utils/streams.js
@@ -1,23 +1,74 @@
+const fs = require('fs');
 const program = require('commander');
+const through = require('through2');
+const csvjson = require('csvjson');
+const config = require('../config/config.json');
 
-const reverse = str => {
-  console.log(`run reverse(${str})`)
+/** node utils/streams.js --action reverse */
+const reverse = () => {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(data.toString().split('').reverse().join(''));
+  })
 };
 
-const transform = str => {
-  console.log(`run transform(${str})`)
+
+/** node utils/streams.js --action=transform */
+const transform = (str) => {
+  const transformStream = through(write, end);
+
+  function write(chunk, enc, next) {
+    const transformedChunk = chunk.toString().toUpperCase();
+    this.push(transformedChunk);
+    next();
+  }
+
+  function end (done) {
+    done();
+  }
+
+  process.stdin.pipe(transformStream).pipe(process.stdout);
 };
 
+/** node utils/streams.js --action=outputFile --file=./utils/textFile.txt */
 const outputFile = filePath => {
-  console.log(`run outputFile(${filePath})`)
+  const reader = fs.createReadStream(filePath);
+  
+  reader.on('data', chunk => {
+    process.stdout.write(chunk.toString())
+  })
 };
 
+/** node utils/streams.js --action=convertFromFile --file=./utils/csvFile.csv */
 const convertFromFile = filePath => {
-  console.log(`run convertFromFile(${filePath})`)
+  const reader = fs.createReadStream(filePath);
+  let data = '';
+  
+  reader.on('data', chunk => {
+    data += chunk;
+  })
+  
+  reader.on('end', () => {
+    const jsonData = csvjson.toObject(data, config.csv_option);
+    process.stdout.write(JSON.stringify(jsonData));
+  })
 };
 
+/** node utils/streams.js --action=convertToFile --file=./utils/csvFile.csv */
 const convertToFile = filePath => {
-  console.log(`run convertToFile(${filePath})`)
+  const reader = fs.createReadStream(filePath);
+  const newFilePath = filePath.replace('.csv', '.json');
+  const writer = fs.createWriteStream(newFilePath);
+  
+  let data = '';
+
+  reader.on('data', chunk => {
+    data += chunk;
+  });
+  
+  reader.on('end', () => {
+    jsonData = csvjson.toObject(data, config.csv_option);
+    writer.write(JSON.stringify(jsonData));
+  });
 };
 
 const functions = {
