2999adf3e4443d2250bd9d9761dda0ec6b57a8c9
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-06-15 17:21:42 +0300

    remove  redundant code

--- app_new.js
+++ app_new.js
@@ -13,17 +13,10 @@ app.use(express.json());
 app.use(passport.initialize());
 app.use(passport.session());
 
-// app.use(lightCookieParser());
-// app.use(ligntQueryParser());
-
 routes(app);
 
 app.get('/', (req, res) => {
 
-  // res.write(JSON.stringify(req.parsedCookies));
-  // res.write(JSON.stringify(req.parsedQuery));
-  // console.log(req.parsedCookies);
-  // console.log(req.parsedQuery);
 });
 
 export default app;
