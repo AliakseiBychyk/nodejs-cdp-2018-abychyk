001cd8a7a11ae2b8c43547b70a53a7984f7f26d9
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-06-21 12:59:23 +0300

    OAuth 2 Scopes for Google APIs

--- controllers/authControllers/googleController.js
+++ controllers/authControllers/googleController.js
@@ -4,8 +4,11 @@ import '../../config/passportGoogleAuthStrategy';
 export const googleAuth = (req, res, next) => {
   passport.authenticate('google',
     {
-      // scope: ['https://www.googleapis.com/auth/plus.login'], // reques to `View your language preferences`, `View your approximate age`
+      // https://developers.google.com/identity/protocols/googlescopes
+
+      // scope: 'https://www.googleapis.com/auth/plus.login', // reques to `View your language preferences`, `View your approximate age`
       // scope: 'https://www.google.com/m8/feeds', // request to `Manage your contacts`
+      // scope: 'https://www.googleapis.com/auth/drive', // `View and manage the files in your Google Drive`
       scope: ['profile'], // without any additional request form
     }
   )(req, res, next);
