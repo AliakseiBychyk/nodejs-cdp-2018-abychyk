f5b9c543e6b30714e3d5663f4d67962241bd65c9
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-06-28 16:53:50 +0300

    change callback methods into promise methods

--- controllers/citiesController.js
+++ controllers/citiesController.js
@@ -4,33 +4,30 @@ import cities from '../assets/cities.json';
 
 const Cities = mongoose.model('Cities', CitySchema);
 
-const checkCity = (city, cb) => {
-  Cities.find({ name: city.name }, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else if (data.length > 0) {
-      cb('City already exists', null);
-    } else {
-      city.save((err, data) => {
-        cb(err, city);
-      });
-    }
-  });
+const checkCity = (city) => {
+  return Cities.find({ name: city.name })
+    .then(result => {
+      if (result.length > 0) {
+        console.log('City already exists');
+        return {};
+      }
+      city.save();
+      return city;
+    })
+    .catch(err => console.log(err));
 };
-
 
 export const postCities = (req, res) => {
   const newCity = new Cities({ ...req.body });
-  checkCity(newCity, (err, city) => {
-    if (err || !city) return res.json({ status: 500, error: err });
-    res.json(city);
-  });
-
+  checkCity(newCity)
+    .then(city => {
+      res.json(city);
+    })
+    .catch(err => res.send({ status: 500, error: err }));
 };
 
 export const getAllCities = (req, res) => {
-  Cities.find({}, (err, data) => {
-    if (err) return res.send({ status: res.statusCode, error: err });
-    res.json(data);
-  });
+  Cities.find({})
+    .then(data => res.json(data))
+    .catch(err => res.send({ status: res.statusCode, error: err }));
 };
