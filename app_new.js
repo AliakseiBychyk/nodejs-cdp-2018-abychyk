c804b8e3016c1dfb81e88bdc367158f5094aa525
Aliaksei Bychyk <Aliaksei_Bychyk@epam.com>
2018-07-23 12:12:19 +0300

    setup postgres

--- app_new.js
+++ app_new.js
@@ -1,9 +1,14 @@
 import express from 'express';
 import passport from 'passport';
 import mongoose from 'mongoose';
+import { Client } from 'pg';
 import session from 'express-session';
 import routes from './routes/appRoutes';
-import config, {mongodb_uri, mongodb_db} from './config/config.json';
+
+
+import config, {
+  mongodb_uri, mongodb_db, postgres_config, postgres_uri,
+} from './config/config.json';
 
 const app = express();
 
@@ -11,6 +16,11 @@ mongoose.Promise = global.Promise;
 mongoose.connect(`${mongodb_uri}/${mongodb_db}`)
   .then(() => console.log(`Successfully connected to MongoDB on ${mongodb_uri}`))
   .catch(err => console.log(`There was a db connection error\n ${err}`));
+
+const client = new Client(postgres_config);
+client.connect()
+  .then(() => console.log(`Successfully connected to PostgreSQL on ${postgres_uri}`))
+  .catch(err => console.error('connection error', err.stack));
 
 app.use(session({
   secret: 'moon of alabama',
